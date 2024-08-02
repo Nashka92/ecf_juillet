e3359d42d0795661a6084c2aeecf52250e9f7772
Anna RAACH <annagraph92@gmail.com>
2024-08-02 15:09:22 +0200

    feat: installing bootsrap and configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customYellow: '#FCCB01', 
+      },
+    },
   },
+  variants: {},
   plugins: [],
 }
 
