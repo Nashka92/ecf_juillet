0ca0d1d0e631f8e0764e47c8586cc39c669d11ac
Anna RAACH <annagraph92@gmail.com>
2024-08-02 16:47:20 +0200

    feat: icons svg

---
+++ src/asset/icons/NeVousDecouragezPasIcon.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const NeVousDecouragezPasIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="5em"
+    height="5em"
+    viewBox="0 0 256 256"
+  >
+    <path
+      fill="orange"
+      d="M128 24a104 104 0 1 0 104 104A104.13 104.13 0 0 0 128 24M92 96a12 12 0 1 1-12 12a12 12 0 0 1 12-12m76 72H88a8 8 0 0 1 0-16h80a8 8 0 0 1 0 16m-4-48a12 12 0 1 1 12-12a12 12 0 0 1-12 12"
+    />
+  </svg>
+);
+
+export default NeVousDecouragezPasIcon;
